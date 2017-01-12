0108ae59816449f15b32a2fa0fc4d718f19b47b4
Maxwell Chow <maxchowla@gmail.com>
2017-01-11 20:28:41 -0800

    Adding necessary plugins stuff

---
+++ includes/Robot.h
@@ -0,0 +1,39 @@
+/*
+ * Robot.h
+ *
+ * Created 11 Jan 2017
+ *
+ * Header file for Robot.cpp
+ *
+ */
+
+#ifndef INCLUDES_ROBOT_H_
+#define INCLUDES_ROBOT_H_
+
+#include <vector>
+
+class IterativeRobot;
+
+class Robot: public IterativeRobot{
+	public:
+		Robot(); //Constructor for the Robot class.
+		~Robot(); //Destructor for the Robot class.
+	protected:
+
+		void RobotInit() override; //Called when robot is initialized
+		void AutonomousInit() override; //Called when entering Autonomous mode
+		void TeleopInit() override; //Called when entering Teleoperated mode
+		void TestInit() override; //Called when entering test mode
+		void DisabledInit() override; //Called when entering Disabled mode
+
+		void AutonomousPeriodic() override; //Run periodically during Autonomous mode
+		void TeleopPeriodic() override; //same thing as the last one what do you think
+		void TestPeriodic() override;
+		void DisabledPeriodic() override;
+
+		void End(); //Called when robot is done.
+	private:
+		const bool IS_DRIVER_DUMB = true;
+};
+
+#endif /* INCLUDES_ROBOT_H_ */
---
+++ src/Robot.cpp
@@ -0,0 +1,21 @@
+/*
+ * Robot.h
+ *
+ * Created 11 Jan 2017
+ *
+ * Main Robot file
+ *
+ */
+
+#include "WPILib.h"
+#include "Robot.h"
+
+Robot::Robot():
+	IterativeRobot()
+{
+
+}
+
+Robot::~Robot() {
+
+}
